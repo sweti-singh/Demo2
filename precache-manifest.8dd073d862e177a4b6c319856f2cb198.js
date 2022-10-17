2e22f03d98b4db7da9fd095b9572456fad7bafd6
Neeraj Singh <sweti.singh1227@gmail.com>
2022-10-17 12:27:05 +0530

    Updates

---
+++ precache-manifest.8dd073d862e177a4b6c319856f2cb198.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a5377747bf459611aad",
+    "url": "/static/css/main.d6bba81d.chunk.css"
+  },
+  {
+    "revision": "1a5377747bf459611aad",
+    "url": "/static/js/main.1a537774.chunk.js"
+  },
+  {
+    "revision": "5ee476ae1244b517c019",
+    "url": "/static/js/1.5ee476ae.chunk.js"
+  },
+  {
+    "revision": "01125839cec075473419",
+    "url": "/static/css/2.36bc9b46.chunk.css"
+  },
+  {
+    "revision": "01125839cec075473419",
+    "url": "/static/js/2.01125839.chunk.js"
+  },
+  {
+    "revision": "d415269a3c0ab26ca93f",
+    "url": "/static/js/runtime~main.d415269a.js"
+  },
+  {
+    "revision": "145865ccefb8e9958871cfd25f8df74f",
+    "url": "/index.html"
+  }
+];
